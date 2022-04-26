7cfdf7572c38ebc1be4d2723bf98f60c7e2d42ff
Mjchaurasia9 <92732297+Mjchaurasia9@users.noreply.github.com>
2022-04-27 00:57:16 +0530

    Add files via upload

---
+++ pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Enter a Numbet:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+int m = n-1;
+int l = i;
+for (int j = 1; j <= i; j++)
+{
+Console.Write(l+" ");
+l = l+m;
+m--;
+}
+Console.WriteLine();
+}
